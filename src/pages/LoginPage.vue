1b6ef9d6e2bccbad78818eeb1a6cbd2f8f54a4ff
z1zonemoi <zizonemoi@gmail.com>
2024-03-04 22:10:33 +0900

    [core]

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <q-btn label="login" />
+
+    <q-input type="email" v-model="email" />
+
+    <q-input type="password" v-model="password" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const email = ref();
+const password = ref();
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,7 +4,10 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'login', component: () => import('pages/LoginPage.vue') },
+    ],
   },
 
   // Always leave this as last one,
